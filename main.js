307b2a02b1e3eb546062f7e77a592d4362b7068e
AdamHaniff <AdamHaniffDeveloper@gmail.com>
2024-03-17 23:59:38 -0400

    Added click event listener to contact me elements

--- main.js
+++ main.js
@@ -1,2 +1,11 @@
 import "core-js/stable";
 import "regenerator-runtime/runtime";
+
+// VARIABLES
+contactMe = document.querySelectorAll('[href="#contact"]');
+
+contactMe.forEach((el) => {
+  el.addEventListener("click", function (e) {
+    // Have to write code to scroll smoothly to contact section
+  });
+});
