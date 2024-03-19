3937f6bdfc16513f79643467e0d4c48b7bb94519
AdamHaniff <AdamHaniffDeveloper@gmail.com>
2024-03-18 23:59:00 -0400

    Created scrollToContactSection function

--- main.js
+++ main.js
@@ -2,10 +2,21 @@ import "core-js/stable";
 import "regenerator-runtime/runtime";
 
 // VARIABLES
-contactMe = document.querySelectorAll('[href="#contact"]');
+const contactMe = document.querySelectorAll('[href="#contact"]');
+const contactSection = document.querySelector(".contact");
+const sendMessageBtn = document.querySelector(".form__btn");
+const formInput = document.querySelectorAll(".form__input");
 
-contactMe.forEach((el) => {
-  el.addEventListener("click", function (e) {
-    // Have to write code to scroll smoothly to contact section
-  });
-});
+console.log(formInput);
+
+// EVENT LISTENER CALLBACK FUNCTION
+function scrollToContactSection(e) {
+  // Prevent the default behavior of the anchor link
+  e.preventDefault();
+  // Scroll smoothly to the contact section
+  contactSection.scrollIntoView({ behavior: "smooth" });
+}
+
+// EVENT LISTENERS
+contactMe.forEach((el) => el.addEventListener("click", scrollToContactSection));
+sendMessageBtn.addEventListener("submit", function (e) {});
