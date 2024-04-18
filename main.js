edf6b0894b593cd7dd6e84f54f6f5d981d74bb8c
AdamHaniff <AdamHaniffDeveloper@gmail.com>
2024-04-17 21:24:29 -0400

    Updated phone number and LinkedIn link in footer

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-// import "core-js/stable";
-// import "regenerator-runtime/runtime";
+import "core-js/stable";
+import "regenerator-runtime/runtime";
 
 // VARIABLES
 const nameLogo = document.querySelectorAll(".header-footer__name");
